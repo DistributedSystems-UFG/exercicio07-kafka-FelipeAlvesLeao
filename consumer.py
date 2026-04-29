ea236baf75ad80e0d656f71d43dbd423639036d0
Felipe Alves Leão de Araújo <felipealvesleao3@gmail.com>
2026-04-29 19:17:20 -0300

    Add files via upload

--- consumer.py
+++ consumer.py
@@ -1,19 +1,25 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 from const import *
 import sys
 
-# Create consumer: Option 1 -- only consume new events
 consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 
-# Create consumer: Option 2 -- consume old events (uncomment to test -- and comment Option 1 above)
-#consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
 
 try:
-  topic = sys.argv[1]
+    topic = sys.argv[1]
+    processed_topic = sys.argv[2]
 except:
-  print ('Usage: python3 consumer <topic_name>')
-  exit(1)
-  
+    print('Usage: python3 consumer.py <topic> <processed_topic>')
+    exit(1)
+
+producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+
 consumer.subscribe([topic])
+
+print(f'Consuming from "{topic}" and forwarding to "{processed_topic}"...')
+
 for msg in consumer:
-    print (msg.value)
+    print(msg.value)
+    producer.send(processed_topic, value=msg.encode())
+
+producer.flush()
